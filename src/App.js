21272398facc86be382541401c4f8d87aa49e683
青木知美 <tomomi.aoki@gaiax.com>
2019-08-06 19:03:13 +0900

    component creat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <header>
+//         <p>Hello React</p>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = () => {
   return (
     <div>
-      <header>
-        <p>Hello React</p>
-      </header>
+      <Cat />
+      <Cat />
+      <Cat />
     </div>
-  );
+  )
+}
+
+const Cat = () => {
+  return <div>Maw!</div>
 }
 
 export default App;
