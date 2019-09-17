5988adf8c32bb61a1ba1f6bb6f8645c14994ff43
青木知美 <tomomi.aoki@gaiax.com>
2019-09-17 19:27:34 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React,{ Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <header>
-        <p>Hello React</p>
-      </header>
-    </div>
-  );
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.state = { count: 0}
+  }
+
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  handleMinusButton = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render(){
+    return (
+    <React.Fragment>
+    <div>counter: {this.state.count}</div>
+    <button onClick={this.handlePlusButton}>+1</button>
+    <button onClick={this.handleMinusButton}>-1</button>
+    </React.Fragment>
+    )
+  }
 }
 
 export default App;
